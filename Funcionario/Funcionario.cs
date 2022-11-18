dbcc5e6db661a57b5d9d0e5767efbc70a2b23233
HiensenLima <hiensen.lima@sienacompany.com>
2022-11-18 11:47:22 -0300

    aplicação back-end Orientada a Objetos c#

---
+++ Funcionario/Funcionario.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Funcionario
+{
+    internal class Funcionario
+    {
+        private string _Nome;
+        private double _SalarioBruto;
+        private double _Imposto;
+
+        public Funcionario(string nome, double salarioBruto, double imposto)
+        {
+            _Nome = nome;
+            _SalarioBruto = salarioBruto;
+            _Imposto = imposto;
+        }
+
+        public string Nome
+        {
+            get { return _Nome; }   
+            set { _Nome = value; }
+        } 
+
+        public double SalarioBruto
+        {
+            get { return _SalarioBruto; }
+            set { _SalarioBruto = value; }
+        }
+
+        public double Imposto
+        {
+            get { return _Imposto; }
+            set { _Imposto = value; }
+        }
+
+        public double SalarioLiquido()
+        {
+           return _SalarioBruto - _Imposto;
+           
+        }
+
+        public void AumentarSalario(double porcentagemDeAumento)
+        {
+            _SalarioBruto = _SalarioBruto + (_SalarioBruto * (porcentagemDeAumento / 100));
+            
+        }
+    }
+}
---
+++ Funcionario/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Funcionario
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string nome;
+            double salarioBruto;
+            double imposto;
+            double porcentagemDeAumento;
+
+            Console.Write("Digite o seu Nome : ");
+            nome = Console.ReadLine();
+
+            Console.Write("Informe o seu salário bruto (ex: 1.500) : R$");
+            salarioBruto = double.Parse(Console.ReadLine());
+
+            Console.Write("Informe o percentual do imposto : % ");
+            imposto = double.Parse(Console.ReadLine());
+
+            Funcionario funcionario = new Funcionario(nome, salarioBruto, imposto);
+
+            Console.WriteLine("\n=============Dados Funcionário================\n");
+            Console.WriteLine($"Nome : {funcionario.Nome} \nSalário Descontado: {funcionario.SalarioLiquido():c}");
+            Console.WriteLine("\n=============================\n");
+
+            Console.Write("Informe qual a porcentagem de aumento do salário : %");
+            porcentagemDeAumento = double.Parse(Console.ReadLine());
+
+            funcionario.AumentarSalario(porcentagemDeAumento);
+            double novoSalario = funcionario.SalarioLiquido();
+
+            Console.WriteLine("\n===============Salário Atualizado================");
+
+            Console.Write($"Nome : {funcionario.Nome} \nSalário Líquido: {novoSalario:c} \n ");
+
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
